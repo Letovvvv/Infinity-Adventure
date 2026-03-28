0c7f52ef4b396935d0032ca7a9ee5ad3b95b706b
Letovvvv <alex.tat521@gmail.com>
2026-03-28 09:12:09 +0300

    Added movement animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,11 +7,20 @@ public class Player : MonoBehaviour
 
         [SerializeField] private float _initialMovingSpeed = 8f;
 
+        /// <summary>
+        /// то, с какой скоростью и куда движется игрок
+        /// </summary>
         private Vector2 _inputVector;
+        
         private Rigidbody2D _rigidbody2D;
         private float _movingSpeed;
         private float _minMovingSpeed = 0.1f;
+
+        /// <summary>
+        /// минимальное значение x при котором игрок будет повернут вправо
+        /// </summary>
         private float _minTurn = 0.1f;
+
         private bool _isRunning;
         private bool _isTurnLeft;
 
@@ -37,6 +46,19 @@ public class Player : MonoBehaviour
                 HandleMovement();
         }
 
+        public bool IsRunning()
+        {
+                return _isRunning;
+        }
+
+        public bool IsTurnLeft()
+        {
+                return _isTurnLeft;
+        }
+
+        /// <summary>
+        /// изменение позиции игрока и переменных isRunning и isTurnLeft
+        /// </summary>
         private void HandleMovement()
         {
                 _rigidbody2D.MovePosition(_rigidbody2D.position + _inputVector * (_movingSpeed * Time.fixedDeltaTime));
--- Assets/Scripts/Player/PlayerVisual.cs
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class PlayerVisual : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Player _player;
+
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    /// <summary>
+    /// для аниматора
+    /// </summary>
+    private const string IS_RUNNING = "IsRunning";
+
+    private void Awake()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateVisual();
+    }
+
+    /// <summary>
+    /// обновляет анимацию и поворот
+    /// </summary>
+    private void UpdateVisual()
+    {
+        _animator.SetBool(IS_RUNNING, Player.Instance.IsRunning());
+        _spriteRenderer.flipX = Player.Instance.IsTurnLeft();
     }
 }
